92d40ce69e8b5c7b9714f7a25729a2e32c3f0e9c
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-06-20 16:21:58 +0900

    update

--- app/app.py
+++ app/app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template,redirect,request,Blueprint,jsonify
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == "POST":
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        output = firstname + lastname
-        if firstname and lastname:
-            return jsonify({'output':'名前は:' + output + 'です'})
-        return jsonify({'error' : 'Missing data!'})
+        # let request = {
+        # json_ratings: jsonEncoded,
+        # meta: createMeta(),
+        # user_id: userId};
+        return request.get_data()
 
     # page = request.args.get('page',1,type=int)
     # blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
     # return render_template('index.html',blog_posts=blog_posts)
-    return "<div>aaa</div>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
