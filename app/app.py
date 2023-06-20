d1dbfbcb09351b32d0dc66dc3e0d53e0c6fbbdb4
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-06-20 19:43:09 +0900

    static folder

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template,redirect,request,Blueprint,jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 CORS(app)
 
 @app.after_request
