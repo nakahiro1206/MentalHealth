1ed344e08932ae1d23fe0c3058a3802bb0bafcfc
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-10-16 02:30:26 +0900

    animation

--- VisualFeedback/distortion/main.js
+++ VisualFeedback/distortion/main.js
@@ -7,6 +7,9 @@ const wrapper = document.getElementById("wrapper");
 // canvas which draws ball
 const canvas = document.getElementById("canvas");
 const g = canvas.getContext("2d"); // ctx means context
+// dead canvas which draw fixed ball
+const dead_canvas = document.getElementById("canvas_dead");
+const dead_g = dead_canvas.getContext("2d"); // ctx means context
 // start menu
 const start_canvas = document.getElementById("start_canvas");
 const start_g = start_canvas.getContext("2d");
@@ -24,56 +27,81 @@ window.addEventListener("deviceorientation", function(e){
 const balls =Array();
 /*ボールクラス*/
 class Ball{
-	constructor(x, y, r, v){
+	constructor(x, y, r, ink, color){
 		this.x = x;	// x座標
 		this.y = y;	// y座標
 		this.r = r;	// 半径
-		this.v = v; // 速度.
+		this.ink = ink; // 進める量.
+		this.color = color;// color: [read, green, blue, alpha]
 		// color info.
 	}
 	draw(){
-		// 位置を計算
-		const vecX = vec.x * this.v;
-		const vecY = vec.y * this.v;
-		this.v -= Math.sqrt(vecX * vecX + vecY * vecY) * 0.001;
-		if(this.v < 0){this.v = 0;};
-		if(this.x + vecX <= SCREEN_WIDTH - this.r){
-			if(this.x + vecX >= this.r){
-				this.x += vecX;
+		// ink=0で終了.
+		if(this.ink==0){return false;}
+		const tmpX = this.x; const tmpY = this.y;
+		// 枠外判定.
+		if(this.x + vec.x <= SCREEN_WIDTH - this.r){
+			if(this.x + vec.x >= this.r){
+				this.x += vec.x;
 			}else{this.x = this.r;}
 		}else{this.x = SCREEN_WIDTH - this.r;}
-
-		if(this.y + vecY <= SCREEN_HEIGHT - this.r){
-			if(this.y + vecY >= this.r){
-				this.y += vecY;
+		// 枠外判定.
+		if(this.y + vec.y <= SCREEN_HEIGHT - this.r){
+			if(this.y + vec.y >= this.r){
+				this.y += vec.y;
 			}else{this.y = this.r;}
 		}else{this.y = SCREEN_HEIGHT - this.r;}
+
+		// ink を距離に比例して減らす.
+		const diffX = this.x - tmpX; const diffY = this.y - tmpY;
+		this.ink -= Math.sqrt(diffX*diffX + diffY*diffY) * 0.01;
+		if(this.ink < 0){this.ink = 0;};
+
 		// 円を描画（塗りつぶし円）
 		g.beginPath();
-		g.fillStyle = "black";
+		const [red, green, blue, alpha] = this.color;
+		g.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 		g.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
 		g.fill();
+		return true;
 	};
 };
+
+function DrawText(){
+	// 背景.
+	// g.fillStyle = "white";
+	// g.fillRect(0, 0, canvas.width, canvas.height);
+
+	g.fillStyle = "green"; g.font = '50px Roboto medium';
+	const text = "Alice's Adventure in Wonderland Nightmare before Christmas"
+	g.fillText(text, 50, 50);
+}
 
 function DrawBalls(){
 	// frameごとに速度減衰; 時間爆発的に; activeなframe, activeなink ballだけ動かす. dead canvasとか作る;
 	// 粒子を一つにして、それに軌跡を与える方が自然かも.
-	// 画面クリア
-	g.fillStyle = "#ddd";
-	g.fillRect(0, 0, canvas.width, canvas.height);
-
 	// ボールを描く
+	let cnt=0;
 	balls.forEach(element => {
-		element.draw();
+		const flag = element.draw();
+		if(flag == false){
+			cnt++;
+		}
 	});
+	if(cnt == balls.length){
+		return false;
+	}
+	return true;
 }
 
 /*ゲームループ*/
+let requestID;
 function mainLoop(){
-	DrawBalls();
+	const flag = DrawBalls();
 	// 再帰呼び出し
-	requestAnimationFrame(mainLoop);
+	if(flag){
+		requestID = requestAnimationFrame(mainLoop);
+	}
 }
 
 /*起動処理*/
@@ -83,12 +111,17 @@ window.addEventListener("load", function(){
 	// start menu
 	start_canvas.width=SCREEN_WIDTH;start_canvas.height=SCREEN_HEIGHT;
 	start_canvas.style.position="absolute";
-	start_canvas.style.zIndex = 1;
+	start_canvas.style.zIndex = 2;
 
 	// canvas
 	canvas.width = SCREEN_WIDTH; canvas.height = SCREEN_HEIGHT;
 	canvas.style.position="absolute";
-	canvas.style.zIndex = 0;
+	canvas.style.zIndex = 1;
+
+	// canvas
+	dead_canvas.width = SCREEN_WIDTH; canvas.height = SCREEN_HEIGHT;
+	dead_canvas.style.position="absolute";
+	dead_canvas.style.zIndex = 0;
 
 	start_g.fillStyle = "rgba(0, 0, 0, 0.4)";
 	start_g.fillRect(0, 0, canvas.width, canvas.height);
@@ -96,38 +129,30 @@ window.addEventListener("load", function(){
 	start_g.fillStyle = "skyblue";
 	start_g.fillRect(50, 50, 200, 100);
 
-	// generate balls
-	for(let i =0;i<10;i++){
-		balls.push(new Ball(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 20, 0.1*i));
-	}
-	DrawBalls();
-	// text
-	g.font = '50px Roboto medium';
-	g.fillText('Artis', 50, 50);
-	const img = g.getImageData(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-	for(let i=0;i<SCREEN_HEIGHT;i++){
-		for(let j=0;j<SCREEN_WIDTH;j++){
-			// i represents height, j width. 0 ~ 255.
-			// alpha
-			let alpha = img.data[3 + j * 4 + i * SCREEN_WIDTH * 4];
-			// red
-			let red = img.data[j * 4 + i * SCREEN_WIDTH * 4];
-			// green
-			let green = img.data[1 + j * 4 + i * SCREEN_WIDTH * 4];
-			// blue
-			let blue = img.data[2 + j * 4 + i * SCREEN_WIDTH * 4];
-			if(red==0 && green==0 && blue==0){
-				// generate ball
-				balls.push(new Ball(j, i, 5, 0.1));	
-			}		
-		}
-	}
-	DrawBalls();
+	DrawText();
 
 	// click event.
 	button.addEventListener("click", function() {
 		button.style.display="none";
 		start_g.clearRect(0, 0, start_canvas.width, start_canvas.height);
+
+		const img = g.getImageData(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+		for(let i=0;i<SCREEN_HEIGHT;i++){
+			for(let j=0;j<SCREEN_WIDTH;j++){
+				// i represents height, j width. 0 ~ 255.
+				const index = (j + i * SCREEN_WIDTH)*4;
+				const [red,green,blue,alpha]=img.data.slice(index, index + 4);
+				if(alpha!=0){
+					// if color is not transparent.
+					// generate ball
+					balls.push(new Ball(j, i, 1, 10, [red,green,blue,alpha]));	
+				}		
+			}
+		}
+		DrawBalls();
 		mainLoop();
+		cancelAnimationFrame(requestID);
+		console.log("exit animation");
+		// exit button.
 	});
 });
