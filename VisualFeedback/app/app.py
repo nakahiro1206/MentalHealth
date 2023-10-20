09418103b99129471c42a1ffd935ed7b67496288
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-10-21 03:27:29 +0900

    TEMPLATES

--- VisualFeedback/app/app.py
+++ VisualFeedback/app/app.py
@@ -5,7 +5,7 @@ from google.oauth2.service_account import Credentials
 import gspread
 import random
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder="static", template_folder="TEMPLATES")
 # CORS(app)
 
 @app.after_request
