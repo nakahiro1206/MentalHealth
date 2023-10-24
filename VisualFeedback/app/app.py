943bb431a159bdc2e13fc5d317698617d54d22a2
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-10-24 16:40:02 +0900

    app deploy

--- VisualFeedback/app/app.py
+++ VisualFeedback/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,redirect,request,Blueprint,jsonify,url_for
+from flask import Flask, render_template,redirect,request,Blueprint,jsonify,url_for, session
 from flask_cors import CORS
 import json
 from google.oauth2.service_account import Credentials
@@ -6,17 +6,9 @@ import gspread
 import random
 
 app = Flask(__name__, static_folder="static", template_folder="TEMPLATES")
-# CORS(app)
+app.secret_key = "secret"
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
-
-@app.route('/',methods=['GET','POST'])
-def index():
+def open_gs():
     # Google Sheet setting.
     scopes = [
         'https://www.googleapis.com/auth/spreadsheets',
@@ -29,7 +21,21 @@ def index():
     gc = gspread.authorize(credentials)
     spreadsheet_url = "https://docs.google.com/spreadsheets/d/17gEfTOPkqb916jOX2YwETBWtvgX_0XU7MtV3xuZutP0/edit?usp=sharing"
     spreadsheet = gc.open_by_url(spreadsheet_url)
+    return spreadsheet
 
+spreadsheet = open_gs()
+
+# CORS permission
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+# access to / 
+@app.route('/',methods=['GET','POST'])
+def index():
     if request.method == "POST":
         print(spreadsheet.sheet1.get_all_values())
         """let request = {
@@ -72,6 +78,71 @@ def index():
     
     else: return render_template('index.html')
 
+# signin page
+@app.route("/signin", methods=["GET"])
+def signin():
+    return render_template("signin.html")
+
+# authentication
+@app.route("/authentication", methods=["GET", "POST"])
+def authentication():
+    if(request.method == "POST"):
+        # spreadsheet = open_gs()
+        username = request.form["username"]
+        password = request.form["password"]
+        User_list = spreadsheet.worksheet("userlist")
+        Usernames = User_list.col_values(1)
+        Passwords = User_list.col_values(2)
+        Users={}
+        for i in range(min(len(Usernames), len(Passwords))):
+            Users[Usernames[i]] = Passwords[i]
+        if(username in Users):
+            if(password == Users[username]):
+                session["username"] = username
+                return redirect(url_for("disclosure"))# render_template("disclosure.html")
+            else:
+                # wrong password
+                return render_template("signin.html", error="Invalid crediential!")
+        else:
+            # not registered
+            return render_template("signin.html", error="Invalid crediential!")
+
+    # if request method is GET
+    else: return render_template("signin.html")
+
+@app.route('/disclosure')
+def disclosure():
+    if 'username' in session:
+        return render_template('disclosure.html')
+    else:
+        return redirect(url_for('signin'))
+    
+@app.route('/pre-eval', methods=["GET", "POST"])
+def preEval():
+    if(request.method =="POST"):
+        disclosure = request.form["disclosure"]
+        session["disclosure"] = disclosure
+        if 'username' in session:
+            return render_template('pre-eval.html')
+        else:
+            return redirect(url_for('signin'))
+    else: return redirect(url_for('signin'))
+
+@app.route('/post-eval')
+def postEval():
+    if 'username' in session:
+        return render_template('post-eval.html')
+    else:
+        return redirect(url_for('signin'))
+
+@app.route('/feedback', methods=["GET", "POST"])
+def feedback():
+    if 'username' in session:
+        # return render_template('selection.html')
+        return render_template('explosion.html', text=session['disclosure'])
+    else:
+        return redirect(url_for('signin'))
+
 if __name__ == "__main__":
-    print (app.url_map)
-    app.run(debug=True)
+    # print (app.url_map)
+    app.run(debug=True, port=2000)
---
+++ VisualFeedback/app/static/src/explosion.js
@@ -0,0 +1,137 @@
+// height, width
+const SCREEN_WIDTH = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const SCREEN_HEIGHT = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+console.log(SCREEN_HEIGHT * SCREEN_WIDTH);
+
+// wrapper
+const wrapper = document.getElementById("wrapper");
+// canvas which draws ball
+const canvas = document.getElementById("canvas");
+const g = canvas.getContext("2d"); // ctx means context
+// particles_wrapper
+const particles_wrapper = document.getElementById("particles_wrapper");
+// start menu
+const start_canvas = document.getElementById("start_canvas");
+const start_g = start_canvas.getContext("2d");
+// button
+const start_button = document.getElementById("start_btn");
+const exit_button = document.getElementById("exit_btn");
+
+// particles array
+const particles =Array();
+/*ボールクラス*/
+class Particle{
+	constructor(div, x, y, color){
+		this.div = div;
+		this.x = x;	// x座標
+		this.y = y;	// y座標
+		this.color = color;// color: [read, green, blue, alpha]
+		// color info.
+	}
+};
+
+function DrawText(){
+	// 背景.
+	// g.fillStyle = "white";
+	// g.fillRect(0, 0, canvas.width, canvas.height);
+	g.fillStyle = "green"; g.font = '50px Roboto medium';
+	const fontSize = 50; const lineHeight = 1.5;
+	const lines = Text.split("\n");
+	for(let i=0; i<lines.length; i++ ){
+		const line = lines[i] ;
+		let addY=fontSize;
+		if (i!=0){addY += fontSize * lineHeight * i ;}
+		g.fillText( line, 100, 100 + addY, SCREEN_WIDTH-100) ;
+	}
+}
+
+/*起動処理*/
+window.addEventListener("load", function(){
+	// wrapper
+	wrapper.style.position="relative";
+	// start menu
+	start_canvas.width=SCREEN_WIDTH;start_canvas.height=SCREEN_HEIGHT;
+	start_canvas.style.position="absolute";
+	start_canvas.style.zIndex = 2;
+	// start_button
+	start_button.style.display = "block";
+
+	// canvas
+	canvas.width = SCREEN_WIDTH; canvas.height = SCREEN_HEIGHT;
+	canvas.style.position="absolute";
+	canvas.style.zIndex = 1;
+
+	// particle wrapper div
+	particles_wrapper.style.width = SCREEN_WIDTH+"px";
+	particles_wrapper.style.height = SCREEN_HEIGHT+"px";
+	particles_wrapper.style.position="absolute";
+	particles_wrapper.style.zIndex = 0;
+
+	start_g.fillStyle = "rgba(0, 0, 0, 0.4)";
+	start_g.fillRect(0, 0, canvas.width, canvas.height);
+
+	DrawText();
+
+	// click event.
+	start_button.addEventListener("click", function() {
+		start_button.style.display="none";
+		start_g.clearRect(0, 0, start_canvas.width, start_canvas.height);
+		start_canvas.style.display ="none";
+
+		const img = g.getImageData(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+		for(let i=0;i<SCREEN_HEIGHT;i++){
+			for(let j=0;j<SCREEN_WIDTH;j++){
+				// i represents height, j width. 0 ~ 255.
+				const index = (j + i * SCREEN_WIDTH)*4;
+				const [red,green,blue,alpha]=img.data.slice(index, index + 4);
+				if(alpha!=0){
+					// if color is not transparent.
+					// generate ball
+					const particle = document.createElement("div");
+					particle.setAttribute("class", "particle");
+					particle.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha/255})`;
+					particle.style.top = i + "px"; particle.style.left = j + "px";
+					particle.style.position = "absolute";
+					particles_wrapper.appendChild(particle);
+					particles.push(new Particle(particle, j, i, [red,green,blue,alpha]));
+				}		
+			}
+		}
+		g.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.style.display="none";
+
+		particles_wrapper.addEventListener("click",function(event){
+			const clientX = event.clientX; const clientY = event.clientY;
+			// console.log(clientX, clientY);
+			const explosionRadius = 50;
+			let length = particles.length;
+			for(let i=0;i<length;i++){
+				const particle = particles[i];
+				if((clientX-particle.x)**2 + (clientY-particle.y)**2 < explosionRadius**2){
+					// explosion effect.
+					particle.div.addEventListener('transitionend', function() {
+						particle.div.remove();
+					});
+
+					function rand(min, max) {return Math.random() * ((max-min) + 1) + min;}
+					const diffX = Math.floor(rand(80, 150) * Math.cos(Math.random()* 2 * Math.PI));
+      				const diffY = Math.floor(rand(80, 150) * Math.sin(Math.random()* 2 * Math.PI));
+					if(i%100==0)console.log(`translateX( ${diffX} px) translateY( ${diffY} px)`);
+					particle.div.style.transform = `translateX(${diffX}px) translateY(${diffY}px)`;
+					particle.div.style.opacity = "0";
+					particle.div.style.transition="all 2s ease";
+					// particle.div.remove();
+					particles.splice(i,1);
+					length--;i--;
+				}
+			};
+			if(length==0){
+				console.log("end");
+			}
+		});
+		// mainLoop();
+		// 先に実行されるっぽい. let で終了コードを準備.
+		// console.log("exit animation");
+		// exit button.
+	});
+});
