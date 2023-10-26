992906191b52913fcf7517e38a96c025e8864cf3
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2023-10-26 13:14:45 +0900

    .

--- VisualFeedback/app/static/src/explosion.js
+++ VisualFeedback/app/static/src/explosion.js
@@ -1,6 +1,10 @@
 // height, width
 const SCREEN_WIDTH = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 const SCREEN_HEIGHT = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+const WRAPPER_WIDTH = document.getElementById("wrapper").clientWidth;
+const WRAPPER_HEIGHT = document.getElementById("wrapper").clientHeight;
+const WRAPPER_LEFT = document.getElementById("wrapper").getBoundingClientRect().left;
+const WRAPPER_TOP = document.getElementById("wrapper").getBoundingClientRect().top;
 console.log(SCREEN_HEIGHT * SCREEN_WIDTH);
 
 // wrapper
@@ -10,15 +14,12 @@ const canvas = document.getElementById("canvas");
 const g = canvas.getContext("2d"); // ctx means context
 // particles_wrapper
 const particles_wrapper = document.getElementById("particles_wrapper");
-// start menu
-const start_canvas = document.getElementById("start_canvas");
-const start_g = start_canvas.getContext("2d");
 // button
 const start_button = document.getElementById("start_btn");
 const exit_button = document.getElementById("exit_btn");
 
 // particles array
-const particles =Array();
+const particles ={};
 /*ボールクラス*/
 class Particle{
 	constructor(div, x, y, color){
@@ -34,55 +35,45 @@ function DrawText(){
 	// 背景.
 	// g.fillStyle = "white";
 	// g.fillRect(0, 0, canvas.width, canvas.height);
-	g.fillStyle = "green"; g.font = '50px Roboto medium';
+	g.fillStyle = "green"; 
+	g.font = '50px Roboto medium';
 	const fontSize = 50; const lineHeight = 1.5;
 	const lines = Text.split("\n");
 	for(let i=0; i<lines.length; i++ ){
 		const line = lines[i] ;
 		let addY=fontSize;
 		if (i!=0){addY += fontSize * lineHeight * i ;}
-		g.fillText( line, 100, 100 + addY, SCREEN_WIDTH-100) ;
+		g.fillText( line, 0, 0 + addY, WRAPPER_WIDTH) ;
 	}
 }
+
+function rand(min, max) {return Math.random() * ((max-min) + 1) + min;}
 
 /*起動処理*/
 window.addEventListener("load", function(){
 	// wrapper
 	wrapper.style.position="relative";
-	// start menu
-	start_canvas.width=SCREEN_WIDTH;start_canvas.height=SCREEN_HEIGHT;
-	start_canvas.style.position="absolute";
-	start_canvas.style.zIndex = 2;
-	// start_button
-	start_button.style.display = "block";
 
 	// canvas
-	canvas.width = SCREEN_WIDTH; canvas.height = SCREEN_HEIGHT;
-	canvas.style.position="absolute";
+	canvas.width = WRAPPER_WIDTH; canvas.height = WRAPPER_HEIGHT;
+	// canvas.style.position="absolute";
 	canvas.style.zIndex = 1;
 
 	// particle wrapper div
-	particles_wrapper.style.width = SCREEN_WIDTH+"px";
-	particles_wrapper.style.height = SCREEN_HEIGHT+"px";
-	particles_wrapper.style.position="absolute";
+	particles_wrapper.style.width = WRAPPER_WIDTH+"px";
+	particles_wrapper.style.height = WRAPPER_HEIGHT+"px";
+	// particles_wrapper.style.position="absolute";
 	particles_wrapper.style.zIndex = 0;
-
-	start_g.fillStyle = "rgba(0, 0, 0, 0.4)";
-	start_g.fillRect(0, 0, canvas.width, canvas.height);
 
 	DrawText();
 
 	// click event.
 	start_button.addEventListener("click", function() {
-		start_button.style.display="none";
-		start_g.clearRect(0, 0, start_canvas.width, start_canvas.height);
-		start_canvas.style.display ="none";
-
-		const img = g.getImageData(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
-		for(let i=0;i<SCREEN_HEIGHT;i++){
-			for(let j=0;j<SCREEN_WIDTH;j++){
+		const img = g.getImageData(0,0,WRAPPER_WIDTH,WRAPPER_HEIGHT);
+		for(let i=0;i<WRAPPER_HEIGHT;i++){
+			for(let j=0;j<WRAPPER_WIDTH;j++){
 				// i represents height, j width. 0 ~ 255.
-				const index = (j + i * SCREEN_WIDTH)*4;
+				const index = (j + i * WRAPPER_WIDTH)*4;
 				const [red,green,blue,alpha]=img.data.slice(index, index + 4);
 				if(alpha!=0){
 					// if color is not transparent.
@@ -93,38 +84,61 @@ window.addEventListener("load", function(){
 					particle.style.top = i + "px"; particle.style.left = j + "px";
 					particle.style.position = "absolute";
 					particles_wrapper.appendChild(particle);
-					particles.push(new Particle(particle, j, i, [red,green,blue,alpha]));
+					particles[i *WRAPPER_WIDTH + j] =new Particle(particle, j, i, [red,green,blue,alpha]);
 				}		
 			}
 		}
 		g.clearRect(0, 0, canvas.width, canvas.height);
 		canvas.style.display="none";
+		particles_wrapper.style.display="block";
 
 		particles_wrapper.addEventListener("click",function(event){
-			const clientX = event.clientX; const clientY = event.clientY;
-			// console.log(clientX, clientY);
-			const explosionRadius = 50;
-			let length = particles.length;
-			for(let i=0;i<length;i++){
-				const particle = particles[i];
-				if((clientX-particle.x)**2 + (clientY-particle.y)**2 < explosionRadius**2){
-					// explosion effect.
-					particle.div.addEventListener('transitionend', function() {
-						particle.div.remove();
-					});
-
-					function rand(min, max) {return Math.random() * ((max-min) + 1) + min;}
-					const diffX = Math.floor(rand(80, 150) * Math.cos(Math.random()* 2 * Math.PI));
-      				const diffY = Math.floor(rand(80, 150) * Math.sin(Math.random()* 2 * Math.PI));
-					if(i%100==0)console.log(`translateX( ${diffX} px) translateY( ${diffY} px)`);
-					particle.div.style.transform = `translateX(${diffX}px) translateY(${diffY}px)`;
-					particle.div.style.opacity = "0";
-					particle.div.style.transition="all 2s ease";
-					// particle.div.remove();
-					particles.splice(i,1);
-					length--;i--;
+			let length = Object.keys(particles).length;
+			const X = Math.floor(event.clientX-WRAPPER_LEFT); const Y = Math.floor(event.clientY-WRAPPER_TOP);
+			const explosionRadius = Math.floor(Math.min(WRAPPER_HEIGHT, WRAPPER_WIDTH) / 5);
+			console.log(explosionRadius **2);
+			console.log(Object.keys(particles).length);
+			const randArray =Array();
+			for(let i =Y-explosionRadius;i<Y+explosionRadius;i++){
+				for(let j =X-explosionRadius;j<X+explosionRadius;j++){
+					if(i<0 || i>=WRAPPER_HEIGHT){continue;}
+					if(j<0 || j>=WRAPPER_WIDTH){continue;}
+					if((Y-i)**2 +(X-j)**2 >=explosionRadius **2){continue;}
+					randArray.push([i, j]);
 				}
-			};
+			}
+			for(let i = (randArray.length - 1); 0 < i; i--){
+				const r = Math.floor(Math.random() * (i + 1));
+				const tmp = randArray[i];
+				randArray[i] = randArray[r];
+				randArray[r] = tmp;
+			}
+			// for(let i =Y-explosionRadius;i<Y+explosionRadius;i++){
+			// 	for(let j =X-explosionRadius;j<X+explosionRadius;j++){
+			// 		if(i<0 || i>=WRAPPER_HEIGHT){continue;}
+			// 		if(j<0 || j>=WRAPPER_WIDTH){continue;}
+			// 		if((Y-i)**2 +(X-j)**2 >=explosionRadius **2){continue;}
+			for(let r=0;r<randArray.length;r++){
+					const [i, j] = randArray[r];
+					const index = i* WRAPPER_WIDTH + j;
+					if(index in particles){
+						const particle = particles[index];
+						// explosion effect.
+						// particle.div.addEventListener('transitionend', function() {
+						// 	particle.div.remove();
+						// });
+						// setTimeout(()=>{
+						// 別のレイヤーで表現しよう.
+						const diffX = Math.floor(rand(80, 150) * Math.cos(Math.random()* 2 * Math.PI));
+						const diffY = Math.floor(rand(80, 150) * Math.sin(Math.random()* 2 * Math.PI));
+						particle.div.classList.add("opaque");
+						particle.div.style.transform = `translateX(${diffX}px) translateY(${diffY}px)`;//  scaleX(5) scaleY(5)`;
+						// delete particles[index]
+						// },0);})(particle);
+					}
+				}
+			// }
+			console.log(length - Object.keys(particles).length);
 			if(length==0){
 				console.log("end");
 			}
